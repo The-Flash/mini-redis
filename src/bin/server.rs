8d8e7dea8b6e8396d4997b52867d8948fe160c77
Augustine Smith <absaugustineflash@gmail.com>
2025-11-25 06:28:25 +0000

    feat: add server cli

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,15 @@
+use clap::{Parser, arg, command};
+
+#[derive(Parser, Debug)]
+#[command(name="mini-redis-server", version, author,about="A mini Redis server implementation")]
+struct Cli {
+    #[arg(long)]
+    port: Option<u16>,
+}
+
 #[tokio::main]
 pub async fn main() -> () {
+    let cli = Cli::parse();
     println!("Hello, server!");
      ()
 }
